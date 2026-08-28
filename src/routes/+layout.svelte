f58e907c821d506245cacad68e00ce1dc51e14b8
Cloud <cloud.haze000@gmail.com>
2026-08-28 13:33:54 -0700

    Icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,6 @@
 -->
 <script lang="ts">
     import "./layout.css";
-    import favicon from "$lib/assets/favicon.svg";
     import { browser } from "$app/environment";
     import { Theme } from "$lib/utils/Theme.svelte";
     import { deviceDetails } from "$lib/mobile.svelte";
@@ -18,7 +17,6 @@
     $inspect(deviceDetails.isMobile);
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <div
     class={`w-full h-full overflow-clip ${deviceDetails.isMobile ? "is-mobile" : ""}`}
 >
